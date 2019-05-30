823959feaae1418c266294280014bc8ad35d7376
Witanday <witanday.cd@gmail.com>
2019-05-30 19:48:35 -0400

    started to build User Model // mongoose

---
+++ api/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+
+
+// The user schema attributes / Characteristics / fields
+
+
+
+
+
+// Hash password before we even save it in to the database
+
+
+
+//compare password in the database and the one that the user in
