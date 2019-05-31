a465d0610c9addf2b4b74d08c4249e23021288eb
Witanday <witanday.cd@gmail.com>
2019-05-30 20:53:48 -0400

    started frontend boiler plater + mongo lab connection

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require ('mongoose');
 const server = express();
 
-
+mongoose.connect('mongodb+srv://root:fast123@cluster0-qu09c.mongodb.net/test?retryWrites=true&w=majority', function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log(" Connected to the Database")
+    }
+})
 //Middleware
 
 server.use(morgan('dev'));
